ae28ef5a1df0bcb2925274c60241886f997bf7d5
Peter Stubbs <ps258@hotmail.com>
2021-10-24 16:30:17 +0100

    added pump and dashboard checks with minimal configured check items

--- tyk-lint.py
+++ tyk-lint.py
@@ -9,7 +9,7 @@ import argparse, sys
 
 DEBUG=False
 
-# a list of functins that can be used to compare trigger values in GatewayConfigChecks with in with actual values in the config file
+# a list of functins that can be used to compare trigger values in *ConfigChecks with in with actual values in the config file
 def isTrue(compare, acutal):
     return acutal
 
@@ -41,14 +41,14 @@ def isThere(compare, actual):
 #    'health_check.health_check_value_timeouts': {          # this is the config path to check
 #        'checkFn': isGT,                                   # the function that will return true to trigger the message being printed
 #        'compare': 0,                                      # the vaule to compare. Here we compare 'config value is greater than 0'
-#        'getValue': True,                                  # the makes the diagnostic output include the value from the config file
-#                                                           # this next one is the message
+#        'reportValue': True,                               # the makes the diagnostic output include the value from the config file
+#                                                           # "message" is printed when checkFN returns true when the key values is compared with compare
 #        'message': 'is greater than 0. This will panic many versions of the gateway if the API healthcheck endpoint is called'
 #    },
 # in this case we get the following printed when the value in the gateway config is > 0
 # [WARN]Gateway: 'health_check.health_check_value_timeouts' (60) is greater than 0. This will panic many versions of the gateway if the API healthcheck endpoint is called
 
-# define all the checks to perform here
+# Define all the simple checks to perform on the gateway config file
 GatewayConfigChecks = {
     'health_check.enable_health_checks': {
         'checkFn': isTrue,
@@ -58,20 +58,20 @@ GatewayConfigChecks = {
     'health_check.health_check_value_timeouts': {
         'checkFn': isGT,
         'compare': 0,
-        'getValue': True,
+        'reportValue': True,
         'message': 'is greater than 0. This will panic many versions of the gateway if the API healthcheck endpoint is called'
     },
     'hash_key_function': {
         'checkFn': isThere,
         'compare': '',
-        'getValue': True,
-        'message': 'is defined. Check for hash_key_function_fallback and the possibility of lost encrypted certs and keys'
+        'reportValue': True,
+        'message': 'is defined. Check for hash_key_function_fallback and the possibility of lost encrypted certs and keys if it has been changed'
     },
     'health_check_endpoint_name':{
         'checkFn': isNE,
         'compare': '/hello',
-        'getValue': True,
-        'message': 'is defined. Check that /hello as not been renamed'
+        'reportValue': True,
+        'message': 'is defined. /hello has been renamed'
     },
     'analytics_config.enable_detailed_recording': {
         'checkFn': isTrue,
@@ -86,8 +86,26 @@ GatewayConfigChecks = {
     'uptime_tests.config.time_wait': {
         'checkFn': isGT,
         'compare': 25,
-        'getValue': True,
+        'reportValue': True,
         'message': 'is greater than ~25. Uptime tests may never trigger'
+    }
+}
+
+# Define all the simple checks to perform on the pump config file
+PumpConfigChecks = {
+    'dont_purge_uptime_data': {
+        'checkFn': isTrue,
+        'compare': True,
+        'message': 'is set. Uptime checks will never be moved to redis.'
+    }
+}
+
+# Define all the simple checks to perform on the pump config file
+DashboardConfigChecks = {
+    'force_api_defaults': {
+        'checkFn': isTrue,
+        'compare': True,
+        'message': 'is set. tyk-sync will not be able to match up synced policies with APIs.'
     }
 }
 
@@ -97,14 +115,14 @@ def checkVal(config, checkObj, checkPath):
         checkFn = checkObj['checkFn']
         if checkPath in config:
             # check that value against compare.
-            if 'getValue' in checkObj and checkObj['getValue']:
+            if 'reportValue' in checkObj and checkObj['reportValue']:
                 checkObj['Value'] = config[checkPath]
             if checkFn(checkObj['compare'], config[checkPath]):
                 return checkObj['message']
         else:
             # harder to handle missing values. Just deal with the fact that missing booleans are 'False'
             if isinstance(checkObj['compare'], bool):
-                if 'getValue' in checkObj and checkObj['getValue']:
+                if 'reportValue' in checkObj and checkObj['reportValue']:
                     checkObj['Value'] = False
                 # Missing implies False so use that
                 if checkFn(checkObj['compare'], False):
@@ -118,16 +136,16 @@ def checkVal(config, checkObj, checkPath):
         return checkVal(config[firstPath], checkObj, restofPath)
 
 
-def gatewayChecks(g):
-    # various checks on the gateway config
-    for checkName in GatewayConfigChecks:
-        check = GatewayConfigChecks[checkName]
-        result = checkVal(g, check, checkName)
+# check the json of the config file against the given checks
+def SimpleChecks(componentName, jsonConfig, ConfigChecks):
+    for checkName in ConfigChecks:
+        check = ConfigChecks[checkName]
+        result = checkVal(jsonConfig, check, checkName)
         if result:
-            if 'getValue' in check and check['getValue']:
-                print('[WARN]Gateway:',"'"+checkName+"'", '('+str(check['Value'])+')', result)
+            if 'reportValue' in check and check['reportValue']:
+                print('[WARN]' + componentName + ':',"'"+checkName+"'", '('+str(check['Value'])+')', result)
             else:
-                print('[WARN]Gateway:',"'"+checkName+"'", result)
+                print('[WARN]' + componentName + ':',"'"+checkName+"'", result)
     return
 
 
@@ -143,10 +161,20 @@ def main():
 
     # load the files
     if args.gatewayConfig:
-        with open(args.gatewayConfig) as gateway_json:
-            G=json.load(gateway_json)
-            gatewayChecks(G)
-
+        with open(args.gatewayConfig) as gatewayJsonFile:
+            G=json.load(gatewayJsonFile)
+            SimpleChecks('Gateway', G, GatewayConfigChecks)
+            gatewayConfigPresent = True
+    if args.pumpConfig:
+        with open(args.pumpConfig) as pumpJsonFile:
+            P=json.load(pumpJsonFile)
+            SimpleChecks('Pump', P, PumpConfigChecks)
+            pumpConfigPresent = True
+    if args.dashboardConfig:
+        with open(args.dashboardConfig) as dashboardJsonFile:
+            D=json.load(dashboardJsonFile)
+            SimpleChecks('Dashboard', D, DashboardConfigChecks)
+            dashboardConfigPresent = True
 
 if __name__ == "__main__":
     main()
